62e1581ff4d7bf54b078c1d56f97cedc33487128
kam with a k <theblizzzzard@gmail.com>
2016-06-25 11:45:10 -0500

    Add files via upload

---
+++ fahrtocelsius.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void convertor() {
+
+    int x = 0;
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+
+  while (x == 0) {
+      fahr = lower;
+      printf("Fahrenheit\tCelsius\n");
+      x++;
+  }
+
+  while (fahr <= upper) {
+
+      celsius = 5 * (fahr-32) / 9;
+      printf("%d\t\t%d\n", fahr, celsius);
+      fahr = fahr + step;
+  }
+
+}
+
+int main() {
+
+ convertor();
+ return 0;
+
+}
