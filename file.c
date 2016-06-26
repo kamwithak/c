4461c6e557984f1eb5ea8d9d7473f337f1a5c5b4
Kamran Choudhry <theblizzzzard@gmail.com>
2016-06-26 03:15:39 -0400

    Update file.c

--- file.c
+++ file.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
-//contacts name and number saved in file.
-void edit(){
+#include <stdlib.h>
+//username and password logger
+void intro() {
 
-  char *name;
-  int number, x=1;
-  FILE *f;
-  f = fopen("info.txt", "a");
-
-  while (x<=3){
-    printf("\nNew contact name: ");
-    scanf("%s", name);
-    printf("New contact number: ");
-    scanf("%d", &number);
-    fprintf(f, "Name:%s\nPhone #:%d\n\n", name, number);
-    x++;
-  }
-
-  printf("\n");
-  fclose(f);
+  char message[] = "\n       Social Media Account Logger...";
+  printf("%s\n", message);
+  printf("~~~~~~~~~ Press to enter continue ~~~~~~~~~\n");
+  getchar();
 
 }
+int init() {
 
-int main(){
+  char type[20], user[20], pw[20];
+  int limit, x=0;
+  FILE *logger;
+  logger = fopen("log.txt", "w");
+    //how many accounts do you wana log?
+    printf("How many accounts would you like to record? (0 to 10) (0 = EXIT)\n(INPUT INTEGER) // ");
+    scanf("%d", &limit);
 
-  edit();
+    if (limit == 0) {
+      printf("Exiting program...\n");
+      return 0;
+    } else {
+        while (x < limit) {
+
+          printf("\nType of Social Media: ");
+          scanf("%s", type);
+          printf("Username: ");
+          scanf("%s", user);
+          printf("Password: ");
+          scanf("%s", pw);
+
+          fprintf(logger, "Type of Social Media:%s\nUsername:%s\nPassword:%s\n\n", type, user, pw);
+          x++;
+
+        }
+          fclose(logger);
+          printf("\nSuccessfully Logged...\nFile -> log.txt\nExiting program...\n\n");
+          return 0;
+      }
+}
+int main() {
+  intro();
+  init();
   return 0;
-
 }
