df63cd18b4332886fed2c65f1195277e6d7b21e0
Kamran Choudhry <theblizzzzard@gmail.com>
2016-07-26 17:43:52 -0400

    Update atm.c

--- atm.c
+++ atm.c
@@ -3,33 +3,33 @@
 #include <string.h>
 
 /* LOGIN SYSTEM, ATM SIMULATION, MANIPULATION OF FILE I/O USING C
-~ KAMRAN WITH A K, LAST UPDATED JULY 24th, 2016 ~  */
+~ KAMRAN WITH A K, LAST UPDATED JULY 26th, 2016 ~  */
 
+void init();
+void options();
 int framework(int option);
 float add_to_chequings(float nChequings, float iChequings, float addChequings);
-float add_to_savings(float nSavings, float iSavings, float addSavings);
 float sub_from_chequings(float nChequings, float iChequings, float subChequings);
+float add_to_savings(float nSavings, float iSavings, float addSavings);
 float sub_from_savings(float nSavings, float iSavings, float subSavings);
+void transaction_type(float iSavings, float iChequings);
 void withdraw(float iSavings, float iChequings);
 void deposit(float iSavings, float iChequings);
-void proposal(float iSavings, float iChequings);
-void finalInfo();
+void final_info();
 void receipt();
-void read();
-void loggedIn();
-void write();
-void options();
-void login();
-void init();
+void atm();
+void sign_in();
+void create_an_account();
 
 int main(void) {
 
   init();
-  return (0);
+
+  return 0;
 
 }
 
-void init() {
+void init(void) {
 
   char pw[20];
   iRestart:
@@ -40,90 +40,64 @@ void init() {
   printf("\nWHAT IS THE START-UP ATM KEY?\n -> ");
   scanf("%s", pw);
 
-    if (strcmp(pw, "885265om") == 0) {  // START-UP ATM KEY = "885265om"
+  if (strcmp(pw, "1234") == 0) {  // START-UP ATM KEY = "1234"
 
-      system("clear");
-      options();
+    system("clear");
+    options();
 
-    } else {
+  }
 
-      goto iRestart;
-
-    }
+  goto iRestart;
 
 }
 
-void options() {
+void options(void) {
 
   int option;
 
   stop:
-  printf("\nWhat would you like to do? (*INPUT INTEGER ONLY*)\n 1 = SIGN IN  2 = CREATE AN ACCOUNT  3 = EXIT\n\n -> ");
+  printf("\nWhat would you like to do? (*INPUT INTEGER ONLY*)\n  1 = SIGN IN  2 = CREATE AN ACCOUNT  3 = EXIT\n\n -> ");
   scanf("%d", &option);
+  system("clear");
 
-    if (option == 3) {
+  if (option == 3) {
 
-      system("clear");
-      exit(EXIT_SUCCESS);  //sys.exit() equivalent
+    exit(EXIT_SUCCESS);  //sys.exit() equivalent
 
-    } else if (option == 1 || option == 2) {
+  } else if (option == 1 || option == 2) {
 
-      system("clear");
-      framework(option);  //calling framework for atm
+    framework(option);  //calling framework for atm
 
-    } else {
+  } else {
 
-      system("clear");
-      goto stop;
+    goto stop;
 
-    }
+  }
 
 }
 
 int framework(int option) {
 
-    if (option == 1) {
+  if (option == 1) {
+	
+	  sign_in();
 
-      read(); //sign in
+  } else {
 
-    } else {
+    create_an_account();
 
-      write(); //create an account
+  }
 
-    }
-
-  return (0);
-
-}
-
-void write() {   //create an account
-
-  char user[50], pwd[50];
-  printf("MENU ~ CREATE AN ACCOUNT\n");
-  printf("------------------------");
-  printf("\nUsername: ");
-  scanf("%s", user);
-  printf("PIN #: ");
-  scanf("%s", pwd);
-
-  FILE *file;
-  file = fopen("accounts.csv", "a");
-
-  fprintf(file, "%s,%s,%s\n", user, pwd, "\0");
-  fclose(file);
-  system("clear");
-
-  printf("\nSuccesfully Created Account! ~ \n");
-
-  options();
+  return 0;
 
 }
 
-void read() {   //sign in
+void sign_in(void) {   //sign in
 
   char user[50], pwd[50], line[50];
   printf("MENU ~ SIGN IN\n");
   printf("--------------");
+
   tRestart:
   printf("\nUsername: ");
   scanf("%s", user);
@@ -140,37 +114,52 @@ void read() {   //sign in
     char *USER = strtok(line, ",");
     char *PASSER = strtok(NULL, ",");
 
-      int mUsername = strcmp(user, USER);
-      int mPassword = strcmp(pwd, PASSER);  //return value of 0 iff srtings are same
+    int mUsername = strcmp(user, USER);
+    int mPassword = strcmp(pwd, PASSER);
 
-  //    printf("%d username: %s\n", mUsername, USER);      ~ method of login system ~
-  //    printf("%d password: %s\n", mPassword, PASSER);    ~ strcmp return value and string organized in a nice terminal ~
+    if (mUsername == 0 && mPassword == 0) {
 
-        if (mUsername == 0 && mPassword == 0) {
+      fclose(file);
+      atm();
+      exit(EXIT_SUCCESS); //leave function
 
-          fclose(file);
-          system("clear");
-          loggedIn();
-          exit(EXIT_SUCCESS); //to leave function
-
-        } else {
-
-          continue;
-
-        }
+    }
 
   }
 
   fclose(file);
-  // program only goes past this point iff program goes through entire file and zero matches are found
   system("clear");
   printf("\nINVALID USERNAME OR PIN #, TRY AGAIN...\n");
   goto tRestart;
 
 }
 
-void loggedIn() {
+void create_an_account(void) {   //create an account
 
+  char user[50], pwd[50];
+  printf("MENU ~ CREATE AN ACCOUNT\n");
+  printf("------------------------");
+
+  printf("\nUsername: ");
+  scanf("%s", user);
+  printf("PIN #: ");
+  scanf("%s", pwd);
+
+  FILE *file;
+  file = fopen("accounts.csv", "a");
+
+  fprintf(file, "%s,%s,%s\n", user, pwd, "\0");
+  fclose(file);
+  system("clear");
+
+  printf("\nSuccesfully Created Account! ~ \n");
+  options();
+
+}
+
+void atm(void) {
+
+  system("clear");
   printf("\n * Authentication Successful *");
   printf("\n  * Welcome to the C.C ATM! *");
   printf("\n    ----------$$$----------\n\n");
@@ -178,7 +167,7 @@ void loggedIn() {
   float iChequings;
   float iSavings;
 
-  printf("What is your INITIAL VALUE for each type of account?\n\nCHEQUINGS -> $");
+  printf("INITIAL VALUE for each type of account?\n\nCHEQUINGS -> $");
   scanf("%f", &iChequings);
 
   printf("SAVINGS -> $");
@@ -188,11 +177,11 @@ void loggedIn() {
   float iTotal = iChequings + iSavings;
   printf("INITIAL ACCOUNT VALUE -> $%.2f\n", iTotal);
 
-  proposal(iSavings, iChequings);
+  transaction_type(iSavings, iChequings);
 
 }
 
-void proposal(float iSavings, float iChequings) {
+void transaction_type(float iSavings, float iChequings) {
 
   int CHOICE;
 
@@ -222,11 +211,11 @@ void withdraw(float iSavings, float iChequings) {
 // -> option to perform another transaction or main menu
 
   int CHOICE;
-  char calculation[] = "WITHDRAW";
+  char type[] = "WITHDRAW";
   float subSavings, subChequings;
   float nSavings, nChequings, nTotal;
 
-  printf("\nWhich account would you like to %s from?\n", calculation);
+  printf("\nWhich account would you like to %s from?\n", type);
   printf("      1 = CHEQUINGS  2 = SAVINGS\n\n -> ");
   scanf("%d", &CHOICE);
 
@@ -254,11 +243,11 @@ void deposit(float iSavings, float iChequings) {
 // -> option to perform another transaction or main menu
 
   int CHOICE;
-  char calculation[] = "DEPOSIT";
+  char type[] = "DEPOSIT";
   float addSavings, addChequings;
   float nSavings, nChequings, nTotal;
 
-  printf("\nWhich account would you like to %s money into?\n", calculation);
+  printf("\nWhich account would you like to %s money into?\n", type);
   printf("      1 = CHEQUINGS  2 = SAVINGS\n\n -> ");
   scanf("%d", &CHOICE);
 
@@ -316,7 +305,7 @@ float sub_from_savings(float nSavings, float iSavings, float subSavings) {
 
 }
 
-void finalInfo() {
+void final_info() {
 
   //display all information of new account balances in terminal using for loop
   //"CURRENT ACCOUNT VALUE", keep things consistent
