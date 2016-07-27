4897117c84c1da39fcf7fd54ba3f080704763ae8
Kamran Choudhry <theblizzzzard@gmail.com>
2016-07-26 21:01:02 -0400

    Update multiplier.c

--- multiplier.c
+++ multiplier.c
@@ -1,18 +1,46 @@
- #include <stdio.h>
+#include <stdio.h>
+#include <string.h>
 
-int mult(int x, int y);  //include data types in function prototype
+int mult(int z, int y) {
 
-int main()
-{
-  int x,y;
-  printf( "Please input two numbers to be multiplied: " );
-  scanf( "%d", &x );
-  scanf( "%d", &y );
-  printf( "The product of your two numbers is %d\n", mult(x,y) ); // dont include datatypes when calling function
-  getchar(); 
+	return z*y;
+
 }
 
-int mult (int x, int y) // include data types when creating function
-{
-  return x * y;
+int main(void) {
+
+	int x=0;
+	int n;
+	char intro[] = "Enter #: ";
+
+	for(;;) {
+
+		printf("%s", intro);
+		scanf("%d", &n);
+			break;
+	}
+
+	do {
+
+		x++;
+		printf("\n%d. ", x);
+
+		for(int y=0;y<n;y++) {
+
+			while(1) {
+
+				printf(":-) ");
+					break;
+
+			}
+
+		}
+
+	} while(x<n); 
+
+	int answer = mult(n, n);
+	printf("\n\n%d x %d = %d :-) in total\n\n", n, n, answer);
+
+	return 0;
+	
 }
