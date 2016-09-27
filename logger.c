f6fbf93d503545a5609734f44a929efd82ae4758
Kamran Choudhry <theblizzzzard@gmail.com>
2016-09-27 12:11:56 -0400

    Update logger.c

--- logger.c
+++ logger.c
@@ -10,31 +10,26 @@ void intro() {
 
 }
 int init() {
-
   char type[20], user[20], pw[20];
   int limit, x=0;
   FILE *logger;
   logger = fopen("log.txt", "w");
-    //how many accounts do you wana log?
-    printf("How many accounts would you like to record? (1 to 10) (0 = EXIT)\n(INPUT INTEGER) // ");
-    scanf("%d", &limit);
-
+  //how many accounts do you wana log?
+  printf("How many accounts would you like to record? (1 to 10) (0 = EXIT)\n(INPUT INTEGER) // ");
+  scanf("%d", &limit);
     if (limit == 0) {
       printf("Exiting program...\n");
       return 0;
     } else {
         while (x < limit) {
-
           printf("\nType of Social Media: ");
           scanf("%s", type);
           printf("Username: ");
           scanf("%s", user);
           printf("Password: ");
           scanf("%s", pw);
-
           fprintf(logger, "Type of Social Media:%s\nUsername:%s\nPassword:%s\n\n", type, user, pw);
           x++;
-
         }
           fclose(logger);
           printf("\nSuccessfully Logged...\nFile -> log.txt\nExiting program...\n\n");
@@ -44,5 +39,4 @@ int init() {
 int main() {
   intro();
   init();
-  return 0;
 }
